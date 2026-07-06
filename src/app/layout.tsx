b7ee1f57090aa2b063958831cd24ce07e7f38d24
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-07-06 20:38:18 +0600

    chore: add talentapp project verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "MiniDrive - DePIN Storage",
   description: "DePIN storage network for MiniPay.",
+  other: {
+    "talentapp:project_verification": "a9877b769725facd7e8fe1bf2014f86c2a27d00c8f83748922c14ec88697fa71b4ad506b5b9a7197bae118e5d687edea4d5980c40148a1f92aa8d38668dc5902"
+  }
 };
 
 import Providers from "@/components/Providers";
